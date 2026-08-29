f1cfbc144ee6cf1879e427206af8b340eecb04c3
Prisha Priyadarshini <150696909+priyadp1@users.noreply.github.com>
2026-08-29 17:53:14 -0400

    Decreased sample size

--- examples/benchmark/olmo_pretrain_benchmark.py
+++ examples/benchmark/olmo_pretrain_benchmark.py
@@ -25,7 +25,7 @@ import torch
 if torch.cuda.is_available():
     torch.cuda.set_device(int(os.environ.get("LOCAL_RANK", "0")))
 
-MAX_SAMPLES = 10000  # subset for continued pretraining
+MAX_SAMPLES = 1000  # subset for continued pretraining
 MAX_SMILES_LENGTH = 200  # cap on longer SMILES strings to avoid OOM
 PRETRAINED_DIR = "./olmo_pretrained_backbone"
 CHECKPOINT_DIR = "./olmo_checkpoints_causal_lm"
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max-samples",
         type=int,
-        default=10000,
+        default=1000,
         help="Number of unique UniChem molecules to use for continued "
         "pretraining.")
     parser.add_argument(
